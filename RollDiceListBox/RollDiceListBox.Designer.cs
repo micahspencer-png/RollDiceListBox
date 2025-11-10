c6647bbe2c012aa83ad333334b6eefac33dad884
micahspencer-png <micahspencer@isu.edu>
2025-11-09 21:01:49 -0700

    Display is now fixed, fixed bug that wouldn't allow re-rolls,

--- RollDiceListBox/RollDiceListBox.Designer.cs
+++ RollDiceListBox/RollDiceListBox.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            DisplayListBox = new ListBox();
+            RollButton = new Button();
+            ExitButton = new Button();
+            ClearButton = new Button();
+            toolTip1 = new ToolTip(components);
+            SuspendLayout();
+            // 
+            // DisplayListBox
+            // 
+            DisplayListBox.FormattingEnabled = true;
+            DisplayListBox.Location = new Point(12, 8);
+            DisplayListBox.Name = "DisplayListBox";
+            DisplayListBox.Size = new Size(776, 264);
+            DisplayListBox.TabIndex = 0;
+            // 
+            // RollButton
+            // 
+            RollButton.Location = new Point(335, 365);
+            RollButton.Name = "RollButton";
+            RollButton.Size = new Size(147, 73);
+            RollButton.TabIndex = 1;
+            RollButton.Text = "&Roll";
+            toolTip1.SetToolTip(RollButton, "Rolls Dice 1000 times and shows the results");
+            RollButton.UseVisualStyleBackColor = true;
+            RollButton.Click += RollButton_Click;
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(641, 365);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(147, 73);
+            ExitButton.TabIndex = 1;
+            ExitButton.Text = "E&xit";
+            toolTip1.SetToolTip(ExitButton, "Closes the Program");
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(488, 365);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(147, 73);
+            ClearButton.TabIndex = 1;
+            ClearButton.Text = "&Clear";
+            toolTip1.SetToolTip(ClearButton, "Clears the Display");
+            ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
+            // 
+            // RollDiceListBox
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ClearButton);
+            Controls.Add(ExitButton);
+            Controls.Add(RollButton);
+            Controls.Add(DisplayListBox);
+            Name = "RollDiceListBox";
+            Text = "RollOfTheDiceListBox";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox DisplayListBox;
+        private Button RollButton;
+        private Button ExitButton;
+        private Button ClearButton;
+        private ToolTip toolTip1;
     }
 }
--- RollDiceListBox/RollDiceListBox.cs
+++ RollDiceListBox/RollDiceListBox.cs
@@ -1,10 +1,155 @@
 namespace RollDiceListBox
 {
+    //Micah Spencer
+    //RCET2265
+    //Fall 2025
+    //Roll of The Dice List Box Program
+    //https://github.com/micahspencer-png/RollDiceListBox.git
     public partial class RollDiceListBox : Form
     {
         public RollDiceListBox()
         {
             InitializeComponent();
+            DisplayResults();
+        }
+        //Program Logic-------------------------------------------------------------------------------------------------------------------
+
+        private static int[] dice = new int[11];
+        static void Roll()
+        {
+            //sets up variables to roll dice randomly
+            Random Dice1 = new Random();
+            Random Dice2 = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                dice[i] = 0;
+            }
+            
+            //rolls and counts dice 1000 times
+            for (int i = 0; i < 1000; i++)
+            {
+                int roll1 = Dice1.Next(1, 7);
+                int roll2 = Dice2.Next(1, 7);
+                int total = roll1 + roll2;
+
+
+                //totals up each roll and adds a count to the global variable
+                switch (total)
+                {
+                    case 2:
+                        dice[0]++;
+                        break;
+
+                    case 3:
+                        dice[1]++;
+                        break;
+
+                    case 4:
+                        dice[2]++;
+                        break;
+
+                    case 5:
+                        dice[3]++;
+                        break;
+
+                    case 6:
+                        dice[4]++;
+                        break;
+
+                    case 7:
+                        dice[5]++;
+                        break;
+
+                    case 8:
+                        dice[6]++;
+                        break;
+
+                    case 9:
+                        dice[7]++;
+                        break;
+
+                    case 10:
+                        dice[8]++;
+                        break;
+
+                    case 11:
+                        dice[9]++;
+                        break;
+
+                    case 12:
+                        dice[10]++;
+                        break;
+
+                    default:
+                        
+                        break;
+
+                }
+            }
+        }
+        static string text = "test didn't work";
+        static void DisplayResults()
+        {
+            Roll();
+            //sets up the display variables
+            string[] header = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            string vDivide = "|";
+            int pad = 4;
+            int totalWidth = 11 * (pad + vDivide.Length);
+            string hDivide = new string('-', totalWidth);
+            string diceresult = "";
+
+            //sets top of display up
+            diceresult += "                     Roll of the Dice\n";
+            
+            diceresult += hDivide +"\n" ;
+
+            //sets the header for each variable
+            foreach (string letter in header)
+            {
+                diceresult += letter.PadLeft(pad + 1) + vDivide;
+            }
+            diceresult += "\n";
+            
+            diceresult += hDivide + "\n";
+
+            //displays the results of the rolls
+            for (int count = 0; count < 11; count++)
+            {
+                //converts int data from table to string
+                string total = Convert.ToString(dice[count]);
+
+                //sets the data to match up with it's header
+                diceresult += total.PadLeft(pad) + vDivide;
+            }
+            
+            diceresult += "\n" + hDivide;
+            text = diceresult;
+        }
+
+    
+
+
+        //Event Handlers------------------------------------------------------------------------------------------------------------------
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+            
+            DisplayListBox.Items.Clear();
+            DisplayResults();
+            foreach (string line in text.Split("\n")) 
+            {
+                DisplayListBox.Items.Add(line); 
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            DisplayListBox.Items.Clear();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
